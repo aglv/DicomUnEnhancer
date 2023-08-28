fd6f18837c856f6022523f5e654e4d5dcf61b5f7
Alessandro Volz <alessandro.volz@gmail.com>
2023-08-28 14:34:20 +0200

    dcm2niix naming convention change

--- DicomUnEnhancer/Scripts/dcm2niix/Sync.sh
+++ DicomUnEnhancer/Scripts/dcm2niix/Sync.sh
@@ -5,7 +5,7 @@ set -e; set -x
 source_dir="$PROJECT_DIR/$PROJECT_NAME/ThirdParty/$TARGET_NAME"
 install_dir="$TARGET_TEMP_DIR/Install"
 
-versioned_filename="dcm2niix_${NATIVE_ARCH}_$(cd "$source_dir" && git describe --tags --dirty)"
+versioned_filename="dcm2niix-${NATIVE_ARCH}-$(cd "$source_dir" && git describe --tags --dirty)"
 
 rsync -avz "$install_dir/bin/dcm2niix" "$TARGET_BUILD_DIR/$versioned_filename"
 
